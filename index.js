a8daf59dad1a540dc6c1d9e505dd4d5354ff7a6c
UdhayaIT <157974716+UdhayaIT@users.noreply.github.com>
2025-04-23 10:43:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let array = [15,117,221,332,123,221];
+let max = INT_MIN;
+for (let index = 0; index < array.length; index++) {
+    if(array[i]>max)
+    {
+        max = array[i];
+    }
+}
+let count =0;
+for(let i=0;i<array.length;i++){
+    
+}
